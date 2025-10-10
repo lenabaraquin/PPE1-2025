ae666d1a7e7d0dbe09f35623497e7c7b6d83703f
Lena Baraquin <baraquin.lena@gmail.com>
2025-10-10 23:56:48 +0200

    Aïe ! j'ai mal lu la consigne (compte_par_type.sh) 😬

--- Exercices/compte_par_type.sh
+++ Exercices/compte_par_type.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 ANNOTATION_PATH="../data/archive-41/ann/"
 
-YEAR=2016
-ANNOTATION_FILES=$ANNOTATION_PATH$YEAR                                    #On concatène l'année au path du répertoire ann pour obtenir le path du répertoire correspondant à une année donnée.
-cd $ANNOTATION_FILES                                                      #On se rend dans le répertoire contenant les annotations pour l'année définie (2016).
-for TAG in Location Date Organization Product Hour Person Event; do       #Liste des étiquettes possible
-  echo Annotations $TAG pour l annee $YEAR : $(cat * | grep $TAG | wc -l) #On lit tous les fichiers du répertoire avec cat, on selectionne les lignes contenant le tag courant avec grep et on compte ces lignes avec wc -l.
-done
+YEAR=$1                                #L'année est donnée par le premier argument du script
+ANNOTATION_FILES=$ANNOTATION_PATH$YEAR #On concatène l'année au path du répertoire ANNOTATION_PATH pour obtenir le path du répertoire correspondant à l'année renseignée.
+TAG=$2                                 #L'étiquette est donnée par le second argument du script
+cd $ANNOTATION_FILES                   #On se rend dans le répertoire contenant les annotations pour l'année renseignée.
+echo $(cat * | grep $TAG | wc -l)      #On lit tous les fichiers du répertoire avec cat, on selectionne les lignes contenant le tag renseigné avec grep et on compte ces lignes avec wc -l.
