b1473747267e7b17528be40264c98a4812bf3669
Lena Baraquin <baraquin.lena@gmail.com>
2025-10-12 00:55:50 +0200

    compte_lieux.sh : fonction pour compter les lieux mais elle marche pas 😬

--- Exercices/compte_lieux.sh
+++ Exercices/compte_lieux.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
+function count_items() {
+  local LAST=""
+  local COUNTER=0
+  LIST = $1
+  while read ITEM; do
+    if ($ITEM==$LAST); then
+      $COUNTER++
+    else
+      echo -e "$COUNTER\t $LAST"
+    fi
+  done < $LIST
+  return
+}
+
 ANNOTATION_PATH="../data/archive-41/ann/"
 
 YEAR=$1
 MONTH=$2
 NB_OUTPUT=$3
 
-ANNOTATION_PATH=$ANNOTATION_PATH$YEAR
-cd $ANNOTATION_PATH
-echo $(cat $YEAR\_$MONTH* | grep Location | awk '{FS="\t"; if ($2 ~ /Location.*/) {print $3}}')
+cd $ANNOTATION_PATH$YEAR
+echo $(cat $YEAR\_$MONTH* | awk '{FS="\t"; if ($2 ~ /Location.*/) {print $3}}' | sort | count_items)
