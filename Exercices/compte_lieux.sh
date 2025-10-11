64a77cf8965fbfcd127eb197266b2c4fae6786a4
Lena Baraquin <baraquin.lena@gmail.com>
2025-10-11 12:30:39 +0200

    début compte_lieux.sh

---
+++ Exercices/compte_lieux.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ANNOTATION_PATH="../data/archive-41/ann/"
+
+YEAR=$1
+MONTH=$2
+NB_OUTPUT=$3
+
+ANNOTATION_PATH=$ANNOTATION_PATH$YEAR
+cd $ANNOTATION_PATH
+echo $(cat $YEAR\_$MONTH* | grep Location | cat)
