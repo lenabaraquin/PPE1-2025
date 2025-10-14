c59f9bd0442e90d9300c07ad5239552c535adccc
Lena Baraquin <baraquin.lena@gmail.com>
2025-10-14 23:04:30 +0200

    vérification des arguments du script compte_lieux.sh le script n'accepte pas encore le * pour le second argument

--- Exercices/compte_lieux.sh
+++ Exercices/compte_lieux.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
-
 ANNOTATION_PATH="../data/archive-41/ann/"
 
+if [[ !($1 =~ [0-9]{4}) ]]; then
+  echo "Renseigner l'année en premier argument, doit être un entier de quatre chiffres."
+fi
+if [[ !($2 =~ ([0-9]{2}|\*)) ]]; then
+  echo "Renseigner le mois en second argument, doit être un entier de deux chiffres."
+fi
+if [[ !($3 =~ [0-9]+) ]]; then
+  echo "Renseigner le nombre de lieux à afficher, doit être un entier."
+fi
+
 YEAR=$1
-MONTH=$2
 NB_OUTPUT=$3
+if [[ $2 =~ [0-9]{2} ]]; then
+  MONTH=$2
+else
+  MONTH=""
+fi
 
 cd $ANNOTATION_PATH$YEAR
 echo $(cat $YEAR\_$MONTH* | awk '{FS="\t"; if ($2 ~ /Location.*/) {print $3}}' | sort | uniq -c | sort -rn | head -n $3)
