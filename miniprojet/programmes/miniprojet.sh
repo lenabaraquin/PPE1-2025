0f14afe2dcc27241e1f08b9f254a626cb99894fb
Lena Baraquin <baraquin.lena@gmail.com>
2025-10-25 18:57:58 +0200

    le script miniprojet.sh renvoie une sortie mais incomplète (cf journal.md).

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -1,4 +1,15 @@
-while read -r line;
-do
-	echo ${line};
-done < "urls/fr.txt";
+#!/bin/bash
+
+if [[ !($# == 1) ]]; then
+  echo "Passer le chemin en paramètre"
+  exit
+fi
+
+path=$1
+n=0
+while read -r line; do
+  http_code=$(curl -is ${line} | grep "HTTP/2" | cut -f 2 -d " ")
+  encodage=$(curl -is ${line} | grep "content-type:" | cut -f 2 -d "=")
+  echo -e "${n}\t${line}\t${http_code}\t${encodage}"
+  n=$(expr $n + 1)
+done <$path
