5bf1a1ef58427f7ac22939f592d28510d91ed0d0
Lena Baraquin <baraquin.lena@gmail.com>
2025-11-11 15:55:29 +0100

    miniprojet 2

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -5,6 +5,16 @@ if [[ !($# == 1) ]]; then
   exit
 fi
 
+echo '''
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <table>
+      <tr><th>id</th><th>url</th><th>code HTTP</th><th>encodage</th><th>nombre de mots</th></tr>
+'''
 path=$1
 n=0
 while read -r line; do
@@ -12,8 +22,15 @@ while read -r line; do
   http_code=$(grep "HTTP/2" tmp.headers | cut -f 2 -d " ")
   encodage=$(grep "charset=" tmp.headers | cut -f 2 -d "=" | tr -d '\r') #Le tr permet de régler un problème de formatage de la sortie (le nombre de mot s'affichait dans la colonne des urls en écrasant le début de la chaine)
   nb_mots=$(lynx -dump -nolist tmp.html | wc -w)
-  echo -e "${n}\t${line}\t${http_code}\t${encodage}\t${nb_mots}"
+  echo "        <tr>"
+  echo -e "          <td>${n}</td><td>${line}</td><td>${http_code}</td><td>${encodage}</td><td>${nb_mots}</td>"
+  echo "        </tr>"
   n=$(expr $n + 1)
 done <$path
 
+echo '''
+    </table>
+  </body>
+</html>
+'''
 rm tmp.headers tmp.html
