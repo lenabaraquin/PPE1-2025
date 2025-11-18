11010cf87cbc0e031823962466b2bc02ef5f48d5
Lena Baraquin <baraquin.lena@gmail.com>
2025-11-18 13:54:05 +0100

    déplacement parametre bulma has-background-grey dans la balise body

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -14,8 +14,8 @@ echo '''
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
   </head>
-  <body>
-    <section class="section has-background-grey">
+  <body class="has-background-grey">
+    <section class="section">
       <div class="container has-background-white">
         <div class="hero has-text-centered">
           <div class="hero-body">
